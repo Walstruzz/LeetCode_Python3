9223f52e5af1b0aaeffb3943a27c7a35c53a0117
walstruzz <1193255670@qq.com>
2019-05-07 10:00:51 +0800

    .

---
+++ post.py
@@ -0,0 +1,21 @@
+import os
+
+for file in os.listdir("."):
+    if not file.endswith(".md") or file == "README.md":
+        continue
+    with open( file, encoding="utf-8") as f:
+        lines = f.readlines()
+    if lines[0].startswith("---"):
+        continue
+    title = lines[0][5:].strip()
+    top, title = title.split(".")
+    add_line = ("---\n"
+                       "title: {}\n"
+                       "data: {}\n"
+                       "top: {}\n"
+                       "categories: Leetcode_python3\n"
+                       "---\n").format(title, "{{ date }}", top)
+    lines = [add_line] + lines
+    with open(file, "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
